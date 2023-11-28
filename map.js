cad4ff36dfd81867fd69693c4e3e93bc006e0392
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-28 12:09:38 +0600

    map

--- map.js
+++ map.js
@@ -1,16 +1,8 @@
-const numbers = [11,22,33,44,55];
+const numbers = [3,4,5,6,7,];
 const doubled = [];
 for(const number of numbers){
-    const double = number * 2;
+    const double = number *2;
     doubled.push(double)
 }
+
 console.log(doubled);
-
-function doubleIt(num){
-    console.log('Num Now', num);
-    return num*2;
-}
-const result = numbers.map(doubleIt)
-console.log(result);
-
-const nums = [11,22,33]
